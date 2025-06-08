546215ba2c6a2e0849c4277b2570413e8ad21952
bdjm <danmoroz55@gmail.com>
2025-06-08 22:30:43 +0300

    Add both dir motion

--- Machine_of_Misha/src/main.cpp
+++ Machine_of_Misha/src/main.cpp
@@ -1,50 +1,59 @@
 #include <Arduino.h>
 
-#define USEC_IN_SEC    (1000000)
+#define USEC_IN_SEC       (1e6)                    // Set's us in sec
 
-#define PUL_PIN        (13)                        // Responsible for the motor's motion
-#define DIR_PIN        (12)                        // Responsible for direction of the motor's motion
-#define ENA_PIN        (11)                        // Responsible for enabling the motor
+#define ENABLE_MOTOR      (1)                          // Enable the motor constant
+#define DISABLE_MOTOR     (0)                          // Disable the motor constant
 
-#define FREQUENCY      (1000)                      // Frequency of the motor's motion, Hz
-#define PERIOD         (USEC_IN_SEC / FREQUENCY)   // Period of one impulse of the PUL signal, us
+#define PULSE_HIGH        (1)                          // Define high level for pulse
+#define PULSE_LOW         (0)                          // Define low level for pulse
 
-#define WAIT_INTERVAL  (5)                         // An interval, while waiting of setting up new signal, us
+#define MOTOR_0_PUL_PIN   (13)                         // Responsible for the motor's motion
+#define MOTOR_0_DIR_PIN   (12)                         // Responsible for direction of the motor's motion
+#define MOTOR_0_ENA_PIN   (11)                         // Responsible for enabling the motor
+
+#define FREQUENCY         (10000)                       // Frequency of the motor's motion, Hz
+#define PERIOD            (USEC_IN_SEC / FREQUENCY)    // Period of one impulse of the PUL signal, us
+
+#define WAIT_INTERVAL     (5)                          // An interval, while waiting of setting up new signal, us
+
+#define STEP_IN_ONE_DIR   (10000)
+
+bool motor_0_dir = false;
 
 void setup()
 {
   // Setting up pins to output mode
-  pinMode(PUL_PIN, OUTPUT);
-  pinMode(DIR_PIN, OUTPUT);
-  pinMode(ENA_PIN, OUTPUT);
+  pinMode(MOTOR_0_PUL_PIN, OUTPUT);
+  pinMode(MOTOR_0_DIR_PIN, OUTPUT);
+  pinMode(MOTOR_0_ENA_PIN, OUTPUT);
+
+  // Enable motor
+  digitalWrite(MOTOR_0_ENA_PIN, ENABLE_MOTOR);
 }
 
 void loop()
 {
-  // Enable motor
-  digitalWrite(ENA_PIN, 0);
-
   // Wait before setting up new signal (according to docs)
   delayMicroseconds(WAIT_INTERVAL);
 
   // Set direction of the motor's motion
-  digitalWrite(DIR_PIN, 0);
+  digitalWrite(MOTOR_0_DIR_PIN, motor_0_dir);
 
   delayMicroseconds(WAIT_INTERVAL);
 
-  // Create impulses on PUL pin to move the motor
-  digitalWrite(PUL_PIN, 1);
+  for (size_t i = 0; i < STEP_IN_ONE_DIR; ++i)
+  {
+    // Create impulses on PUL pin to move the motor
+    digitalWrite(MOTOR_0_PUL_PIN, PULSE_HIGH);
 
-  // Wait a half of period
-  delayMicroseconds((uint32_t)(PERIOD / 2));
+    // Wait a half of period
+    delayMicroseconds((uint32_t)(PERIOD / 2));
 
-  digitalWrite(PUL_PIN, 0);
+    digitalWrite(MOTOR_0_PUL_PIN, PULSE_LOW);
 
-  delayMicroseconds((uint32_t)(PERIOD / 2));
+    delayMicroseconds((uint32_t)(PERIOD / 2));
+  }
 
-  // Disable motor
-  digitalWrite(ENA_PIN, 1);
-
-  // Wait one sec
-  delayMicroseconds(1000000);
+  motor_0_dir = !motor_0_dir;
 }
